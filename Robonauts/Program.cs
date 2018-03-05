56653112ed1aab388d4df58aba9500210e704679
Sudesh Kumar <sudeshkumar1611@gmail.com>
2018-03-05 15:47:41 +0530

    Added Port Number

--- Robonauts/Program.cs
+++ Robonauts/Program.cs
@@ -19,6 +19,7 @@ namespace Robonauts
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5010")
                 .UseStartup<Startup>()
                 .Build();
     }
